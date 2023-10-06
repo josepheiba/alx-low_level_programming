2f947e71e01726102efda87edcd9cfe1b9e85f6c
Youssef Ibba <josepheiba@icloud.com>
2023-10-06 10:54:11 -0700

    ok

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A-to-Z-Dragon Buster Cannon
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 122;
+	while (n > 96)
+	{
+	putchar(n);
+	n--;
+	}
+
+	putchar('\n');
+	return (0);
+}
