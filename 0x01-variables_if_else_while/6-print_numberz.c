71541a90456c940a036cf1d81053efb2e1387105
Youssef Ibba <josepheiba@icloud.com>
2023-10-06 10:08:11 -0700

    ok

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A-to-Z-Dragon Buster Cannon
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	for (; n < 10; n++)
+	{
+	putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
