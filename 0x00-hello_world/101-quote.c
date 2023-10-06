36c72b8f976637f3bf530a0bae2320fce663a2ed
Youssef Ibba <josepheiba@icloud.com>
2023-10-05 17:49:26 -0700

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", sizeof(char), sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"), stdout);
+	return (1);
+}
