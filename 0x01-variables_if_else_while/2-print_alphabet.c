8db8b6f9054c29533958f942547cb2f4bc592a59
Youssef Ibba <josepheiba@icloud.com>
2023-10-06 09:38:53 -0700

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: positive negative zero
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 97;
+	while (n < 123)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
