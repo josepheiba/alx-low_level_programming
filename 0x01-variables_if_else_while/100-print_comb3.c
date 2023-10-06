bdaf7d743476b857eb632f8687fc35fea49e850f
Youssef Ibba <josepheiba@icloud.com>
2023-10-06 16:12:25 -0700

    ok

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: chaos numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int sum;
+	int suminvert;
+	int printed;
+	
+	printed = 0;
+
+	for(i = 0; i < 10; i++)
+	{	
+		for(j = 0; j < 10; j++)
+		{
+		sum = i * 10 + j;
+		suminvert = i + j * 10;
+		if (i != j && sum < suminvert)
+		{
+			if (printed == 1)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+			putchar(i + 48);
+			putchar(j + 48);
+			printed = 1;
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
