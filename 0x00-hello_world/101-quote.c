225d25f8a13c04130acc2f70431d342143399f0c
Youssef Ibba <josepheiba@icloud.com>
2023-10-05 18:19:35 -0700

    letsgoo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char * quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(quote, sizeof(char), sizeof(char) * strlen(quote), stdout);
 	return (1);
 }
