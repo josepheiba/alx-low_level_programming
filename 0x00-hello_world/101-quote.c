1963f22b76736b9fe3bec50b177411ec74457804
Youssef Ibba <josepheiba@icloud.com>
2023-10-05 17:51:09 -0700

    letsgoo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", sizeof(char), sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"), stdout);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(char), sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"), stdout);
 	return (1);
 }
