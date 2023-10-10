2886b929d033b6fbc3c6f5fde4d0bb4fc7205628
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 11:10:40 -0700

    ok

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - description
+ * @c: character
+ * Return: 1 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1)
+	}
+	else
+		return (0);
+}
