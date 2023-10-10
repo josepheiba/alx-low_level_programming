3be97186caf18f86e865e379bf81a8e9832c6dbd
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 13:59:57 -0700

    ok

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - description
+ * @n: number
+ * Return: 1 0
+*/
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for(; n < 98; n++)
+		{
+			printf("%i, ", n);
+		}
+	}
+	else if (n > 98)
+	{	
+		for(; n > 98; n--)
+		{
+			printf("%i, ", n);
+		}
+	}
+	printf("%i\n", n);
+}
