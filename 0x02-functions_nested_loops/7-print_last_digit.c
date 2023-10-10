20a4d0ac1dae8a2daeca98957e6149f314074bcb
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 12:04:43 -0700

    ok

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - description
+ * @n: number
+ * Return: 1 0
+*/
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	if (n < 0)
+		lastDigit = -1 * n % 10;
+	else
+		lastDigit = n % 10;
+	_putchar('0' + lastDigit);
+	return(lastDigit);
+}
