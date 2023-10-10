93a82a2394e32f66278434e41120ae5399cc69a9
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 12:07:20 -0700

    ok

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	if (n < 0)
+	if (n <= 0)
 		lastDigit = -1 * n % 10;
 	else
 		lastDigit = n % 10;
 	_putchar('0' + lastDigit);
-	return(lastDigit);
+	return (lastDigit);
 }
