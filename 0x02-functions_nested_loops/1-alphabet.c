a136e75453a0ec255d32c31b0f8efdf45a247bbe
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 09:53:15 -0700

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char word[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	i = 0;
+	while (word[i] != '\0')
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
