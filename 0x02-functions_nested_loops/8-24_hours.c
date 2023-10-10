a1c5fa093e81b5604ca24b14c4341d90ce1b145e
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 12:31:26 -0700

    ok

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - description
+ * @n: number
+ * Return: 1 0
+*/
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			k = i % 10;
+			l = j % 10;
+			_putchar('0' + (i - k) / 10);
+			_putchar('0' + k);
+			_putchar(':');
+			_putchar('0' + (j - l) / 10);
+			_putchar('0' + l);
+			_putchar('\n');
+		}
+		j = 0;
+	}
+}
