127f3570a54b8b147f318fbad7175494f1919a1b
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 17:33:10 -0700

    ook

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int sum, nbr;
-	
+
 	sum = 0;
-	for(nbr = 0; nbr < 1024; nbr++)
+	for (nbr = 0; nbr < 1024; nbr++)
 	{
 		if (nbr % 3 == 0 || nbr % 5 == 0)
 		{
