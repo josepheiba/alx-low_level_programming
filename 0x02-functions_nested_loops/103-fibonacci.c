de79f568ad912703aec9a1d1fa0789eb3146eda9
Youssef Ibba <josepheiba@icloud.com>
2023-10-10 17:59:11 -0700

    ok

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	unsigned long nbr1, nbr2, nbrnew;
-	int i;
+	int i, sum;
 
+	sum = 0;
 	nbr1 = 0;
 	nbr2 = 1;
 	for (i = 0; i < 50; i++)
@@ -17,12 +18,12 @@ int main(void)
 		nbrnew = nbr1 + nbr2;
 		nbr1 = nbr2;
 		nbr2 = nbrnew;
-		if (i < 49)
+		if (nbrnew > 4000000)
+			break;
+		if (nbrnew % 2 == 0)
 		{
-		printf("%lu, ", nbrnew);
+			sum = sum + nbrnew;
 		}
-		else
-		printf("%lu", nbrnew);
 	}
 	printf("\n");
 	return (0);
