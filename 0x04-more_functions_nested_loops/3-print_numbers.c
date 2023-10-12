ecf0a51a11ded9a37a11ccfbefb71e20d389fb68
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 06:48:43 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: 0 1
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	putchar('\n');
+}
