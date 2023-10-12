90f233e64716d27fb43a548a4e93211b103a6f0a
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 09:31:32 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j >= 10)
 				_putchar('1');
 			_putchar (j % 10 + '0');
 		}
