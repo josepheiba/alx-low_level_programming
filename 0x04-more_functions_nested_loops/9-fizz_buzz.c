4fceef90de3980aa5bff4642a7b7bea809b586f6
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 11:05:15 -0700

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,24 +13,25 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
+			printf("Fizz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
+
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	printf("\n");
 	return (0);
