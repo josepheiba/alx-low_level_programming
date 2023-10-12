e67512646aa487736431fe596be89c0abc51b116
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 08:42:36 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,38 @@
 #include <stdio.h>
 
 /**
+ * _printnum - check the code.
+ * @c : value
+ * Return: 0 1
+ */
+
+void _printnum(int c)
+{
+	int nl, nm, k, l, m;
+	int num[10];
+
+	nl = nm = c;
+	k = 0;
+	do {
+		k++;
+		nl = (nl - (nl % 10)) / 10;
+	} while (nl != 0);
+	for (l = k - 1; l >= 0; l--)
+	{
+		num[l] = (nm % 10);
+		nm = (nm - num[l]) / 10;
+	}
+	for (m = 0; m < k; m++)
+	{
+		_putchar('0' + num[m]);
+	}
+}
+
+/**
  * more_numbers - check the code.
  *
  * Return: 0 1
  */
-
-void _printnum(int c);
 
 void more_numbers(void)
 {
@@ -21,25 +47,4 @@ void more_numbers(void)
 		}
 		putchar('\n');
 	}
-}
-
-void _printnum(int c)
-{
-	int nl, nm, k, l, m;
-	int num[10];
-	nl = nm = c;
-	k = 0;
-        do {
-        	k++;
-		nl = (nl - (nl % 10)) / 10;
-	} while (nl != 0);
-	for (l = k - 1; l >= 0; l--)
-        {
-        	num[l] = (nm % 10);
-        	nm = (nm - num[l]) / 10;
-        }
-        for (m = 0; m < k; m++)
-        {
-        	_putchar('0' + num[m]);
-        }
 }
