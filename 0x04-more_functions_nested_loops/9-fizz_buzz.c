9bd39a5b1fee58671f61d4ea7de6ebc3bd011372
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 11:07:58 -0700

    ok

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,31 +7,24 @@
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 1; i <= 100; i++)
+	for (num = 1; num <= 100; ++num)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (i % 3 == 0)
-		{
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		}
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
-			printf("%d", i);
-		}
+			printf("%d", num);
 
-		if (i != 100)
+		if (num != 100)
 			printf(" ");
 		else
 			printf("\n");
 	}
+
 	return (0);
 }
