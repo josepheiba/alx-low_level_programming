cc3eea0fa2514ff071099fa633d371e107e7d475
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 08:38:08 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,6 +7,8 @@
  * Return: 0 1
  */
 
+void _printnum(int c);
+
 void more_numbers(void)
 {
 	int i, j;
@@ -19,4 +21,25 @@ void more_numbers(void)
 		}
 		putchar('\n');
 	}
+}
+
+void _printnum(int c)
+{
+	int nl, nm, k, l, m;
+	int num[10];
+	nl = nm = c;
+	k = 0;
+        do {
+        	k++;
+		nl = (nl - (nl % 10)) / 10;
+	} while (nl != 0);
+	for (l = k - 1; l >= 0; l--)
+        {
+        	num[l] = (nm % 10);
+        	nm = (nm - num[l]) / 10;
+        }
+        for (m = 0; m < k; m++)
+        {
+        	_putchar('0' + num[m]);
+        }
 }
