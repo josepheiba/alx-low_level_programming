115b539e90c9f75dcd92653fe97394987d18803d
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 16:07:41 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - check the code.
+ * @n : value
+ * Return: 0 1
+ */
+
+void print_number(int n)
+{
+	int nl, nm, k, l, m;
+	int num[10];
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	nl = nm = n;
+	k = 0;
+	do {
+		k++;
+		nl = (nl - (nl % 10)) / 10;
+	} while (nl != 0);
+	for (l = k - 1; l >= 0; l--)
+	{
+		num[l] = (nm % 10);
+		nm = (nm - num[l]) / 10;
+	}
+	for (m = 0; m < k; m++)
+	{
+		_putchar('0' + num[m]);
+	}
+}
