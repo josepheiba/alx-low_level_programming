81d846f81de0e2802b322457cbc3d0b1fe9d3d3f
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 09:12:16 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,35 @@
 #include <stdio.h>
 
 /**
- * more_numbers - dumb version cause checker didn't like the other
+ * _printnum - check the code.
+ * @c : value
+ * Return: 0 1
+ */
+
+void _printnum(int c)
+{
+	int nl, nm, k, l, m;
+	int num[10];
+
+	nl = nm = c;
+	k = 0;
+	do {
+		k++;
+		nl = (nl - (nl % 10)) / 10;
+	} while (nl != 0);
+	for (l = k - 1; l >= 0; l--)
+	{
+		num[l] = (nm % 10);
+		nm = (nm - num[l]) / 10;
+	}
+	for (m = 0; m < k; m++)
+	{
+		_putchar('0' + num[m]);
+	}
+}
+
+/**
+ * more_numbers - check the code.
  *
  * Return: 0 1
  */
@@ -15,15 +43,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-			_putchar('0' + j);
-			}
-			else
-			{
-			_putchar('1');
-			_putchar('0' + (j % 10));
-			}
+			_printnum(j);
 		}
 		putchar('\n');
 	}
