48e3b8dcb5260b83066865a0adc4faa234debcb2
Youssef Ibba <josepheiba@icloud.com>
2023-10-12 17:54:10 -0700

    ok

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,30 +1,6 @@
 #include "main.h"
 
 /**
- * oss - check the code.
- * @e : value
- * @x : value
- * Return: int
- */
-
-int oss(int e, int x)
-{
-	int q;
-	unsigned int E;
-
-	E = e;
-	if (x == 0)
-		return (1);
-	else if (x == 1)
-		return (e);
-	for (q = 1; q < x; q++)
-	{
-		E = E * e;
-	}
-	return (E);
-}
-
-/**
  * print_number - check the code.
  * @n : value
  * Return: 0 1
@@ -32,25 +8,14 @@ int oss(int e, int x)
 
 void print_number(int n)
 {
-	unsigned int k, m;
-	unsigned int nl, nm;
+	unsigned int number = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -number;
 	}
-
-	nl = nm = n;
-	k = 0;
-	do {
-		k++;
-		nl = (nl - (nl % 10)) / 10;
-	} while (nl != 0);
-	for (m = k; m > 0; m--)
-	{
-		nm = n / (oss(10, m - 1));
-		nm = nm % 10;
-		_putchar('0' + nm);
-	}
+	if ((number / 10) > 0)
+		print_number(number / 10);
+	_putchar('0' + (number % 10));
 }
