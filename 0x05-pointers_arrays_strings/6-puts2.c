cfe0d410497647109ef3c54a96daabf8a29ff0b2
Youssef Ibba <josepheiba@icloud.com>
2023-10-16 10:43:37 -0700

    ok

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i + 1] != '\0')
 	{
 		_putchar(str[i]);
-	i+=2;
+	i += 2;
 	}
 	_putchar('\n');
 }
