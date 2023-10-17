a9bd277282b633cc6f7b307ceb7afcd75dfde9d6
Youssef Ibba <josepheiba@icloud.com>
2023-10-16 17:29:42 -0700

    letsgoo

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	int pass[100];
+	int pwd[100];
 	int i, sum, n;
 
 	sum = 0;
@@ -18,9 +18,9 @@ int main(void)
 	srand (time (NULL));
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar (pass[i] + '0');
+		pwd[i] = rand() % 78;
+		sum += (pwd[i] + '0');
+		putchar (pwd[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
