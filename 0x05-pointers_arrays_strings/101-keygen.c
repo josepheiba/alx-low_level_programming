ba0dcb2b52247151741ee64cc18c457023dc745b
Youssef Ibba <josepheiba@icloud.com>
2023-10-16 17:27:01 -0700

    ok

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,10 +18,10 @@ int main(void)
 	srand (time (NULL));
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 1;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar (pass[i] + '0');
-		if ((2772 - sum) - '0' < 1)
+		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
