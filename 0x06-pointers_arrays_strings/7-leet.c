c2bebdae12b45305acb1116d4a0cc901db240f35
Youssef Ibba <josepheiba@icloud.com>
2023-10-18 16:52:01 -0700

    ok

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,23 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char de[] = "aAeEoOtTlL";
+	char *code = "4433007711";
 
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		j = 0;
+		while (de[j] != '\0')
+		{
+			if (s[i] == de[j])
+			{
+				s[i] = code[j];
+				break;
+			}
+		j++;
+		}
 	i++;
 	}
 	return (s);
