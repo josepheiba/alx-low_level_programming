7670413b630fbcc5e5aa27b1d3a95e675ad2e9a6
Youssef Ibba <josepheiba@icloud.com>
2023-10-18 10:58:30 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - check code
+ * @s1: input
+ * @s2: input
+ * Return: check function declaration
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, diff;
+
+	i = diff = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		diff = s1[0] - s2[0];
+	i++;
+	}
+	return (diff);
+}
