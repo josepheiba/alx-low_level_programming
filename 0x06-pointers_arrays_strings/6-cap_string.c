d9248085bf6a73c462288d63b38f1dbc058d4d2d
Youssef Ibba <josepheiba@icloud.com>
2023-10-18 15:52:41 -0700

    okk

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - check code
+ * @s: input
+ * Return: check function declaration
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!'
+				|| s[i] == '?' || s[i] == '"' || s[i] == '('
+				|| s[i] == ')' || s[i] == '{' || s[i] == '}')
+				&& (s[i + 1] < 123 && s[i + 1] > 96))
+		{
+			*(s + i + 1) -= 32;
+		}
+	i++;
+	}
+	return (s);
+}
