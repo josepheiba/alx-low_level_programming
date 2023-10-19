f9a73b4876d0a7a828e0dcd7e0b2a087ea3cbe0d
Youssef Ibba <josepheiba@icloud.com>
2023-10-19 07:33:25 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * infinite_add - check code
+ * @n1: input
+ * @n2: input
+ * @r: input
+ * @size_r: input
+ * Return: check function declaration
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, l, one, two, sum;
+
+	i = j = k = sum = 0;
+	while (n1[i] != '\0')
+	{
+		i++;
+	}
+	while (n2[j] != '\0')
+	{
+		j++;
+	}
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (size_r > l + 1)
+	{
+	for (k = 0; k < size_r; k++)
+	{
+		if (i - k - 1 >= 0)
+			one = (int)n1[i - k - 1] - 48;
+		else
+			one = 0;
+		if (j - k - 1 >= 0)
+			two = (int)n2[j - k - 1] - 48;
+		else
+			two = 0;
+		sum = sum + one + two;
+		r[size_r - k - (size_r - l)] = sum % 10 + '0';
+		if (sum > 9)
+			sum = (sum - (sum % 10)) / 10;
+		else
+			sum = 0;
+	}
+	r[l] = '\0';
+	return (r);
+	}
+	else
+		return (0);
+}
