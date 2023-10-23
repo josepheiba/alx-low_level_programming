84cb1b701d41576d02fb8dfb32227b3684f88dbe
Youssef Ibba <josepheiba@icloud.com>
2023-10-23 06:23:57 -0700

    ok

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return (&s[i]);
+			return (s + i);
 		i++;
 	}
 	if (s[i] == c)
-		return (&s[i]);
+		return (s + i);
 	return ('\0');
 }
