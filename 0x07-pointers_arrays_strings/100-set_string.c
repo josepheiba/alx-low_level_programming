fcf88d03dfc9b9f953f45dc1be88d37c2a407b1a
Youssef Ibba <josepheiba@icloud.com>
2023-10-23 14:04:50 -0700

    ok¡

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - check code
+ * @s: input
+ * @to: input
+ * Return: check function declaration
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
