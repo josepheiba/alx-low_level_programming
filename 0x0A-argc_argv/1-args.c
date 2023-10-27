e623b222be065f0fd6b54085405788d02a7945a4
Youssef Ibba <josepheiba@icloud.com>
2023-10-27 07:31:49 -0700

    ok

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: input
+ * @argv: input
+ * Return: check declaration
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
