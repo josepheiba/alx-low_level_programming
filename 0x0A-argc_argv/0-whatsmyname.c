27198acc7f47ce8ca0b185fcc88b23e3fdeada31
Youssef Ibba <josepheiba@icloud.com>
2023-10-27 07:28:15 -0700

    ok

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: input
+ * @argv: input
+ * Return: check declaration
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", *(argv + 0));
+	return (0);
+}
