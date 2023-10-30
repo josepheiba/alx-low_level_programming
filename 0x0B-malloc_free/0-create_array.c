350f2102d24b59d5c530df0a9b577634fa96c180
Youssef Ibba <josepheiba@icloud.com>
2023-10-30 10:11:40 -0700

    ok

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - check the code.
+ * @size : variable
+ * @c : variable
+ * Return: 0 1
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (0);
+	}
+	i = 0;
+	while (i < size)
+	{
+		*(array + i) = c;
+	i++;
+	}
+	return (array);
+}
