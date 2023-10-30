ec9a521992b72f37ba0cea1634d9dd7eabab8f29
Youssef Ibba <josepheiba@icloud.com>
2023-10-30 10:20:25 -0700

    ok

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,6 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = malloc(sizeof(char) * size);
 
-	if (array == NULL)
-	{
-		return (0);
-	}
 	i = 0;
 	while (i < size)
 	{
