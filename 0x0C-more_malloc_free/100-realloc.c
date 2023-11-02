b323ee2eb938f1854cef19cb4affbee8f2496675
Youssef Ibba <josepheiba@icloud.com>
2023-11-02 12:15:59 -0700

    ok

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,17 +17,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	novoidp = ptr;
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	p = malloc(new_size);
 	if (p == NULL)
-	{
-		free(ptr);
 		return (NULL);
-	}
 
 	i = 0;
 	if (new_size < old_size)
