63fb318738eb2448d8195a118a3234a96aae2547
Youssef Ibba <josepheiba@icloud.com>
2023-11-02 09:13:02 -0700

    ok

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,9 +12,9 @@ int *array_range(int min, int max)
 	int i, l;
 	int *ari;
 
-	l = max - min + 1;
-	if (l < 0)
+	if (min > max)
 		return (NULL);
+	l = max - min + 1;
 	ari = malloc(l * sizeof(int));
 	if (ari == NULL)
 		return (NULL);
