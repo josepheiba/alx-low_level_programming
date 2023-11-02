623c0ac9c70a5b0e34a50c3be627d4ec0cf61fd2
Youssef Ibba <josepheiba@icloud.com>
2023-11-02 09:06:46 -0700

    ok

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * array_range - check the code.
+ * @max : variable
+ * @min : variable
+ * Return: 0 1
+ */
+
+int *array_range(int min, int max)
+{
+	int i, l;
+	int *ari;
+
+	l = max - min + 1;
+	if (l < 0)
+		return (NULL);
+	ari = malloc(l * sizeof(int));
+	if (ari == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i <= l)
+	{
+		*(ari + i) = min + i;
+		i++;
+	}
+	return (ari);
+}
