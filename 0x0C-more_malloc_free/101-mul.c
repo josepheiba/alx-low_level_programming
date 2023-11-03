6ae8c1dc44ef19b53a7c5470a71e3d7775169249
Youssef Ibba <josepheiba@icloud.com>
2023-11-02 17:02:03 -0700

    ok

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,109 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check code
+ * @s: input
+ * Return: check function declaration
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * _isdigit - check the code.
+ * @c : variable
+ * Return: 0 1
+ */
+
+int _isdigit(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] > 47 && c[i] < 58)
+		{
+			return (1);
+			i++;
+		}
+	}
+	return (0);
+}
+
+/**
+ * _print - check the code.
+ * @mul : variable
+ * @l : variable
+ * Return: 0 1
+ */
+
+void _print(int *mul, int l)
+{
+	int x;
+
+	x = 0;
+	if (mul[x] == 0)
+		x = 1;
+	else
+		x = 0;
+	for (; x < l; x++)
+	{
+		_putchar(mul[x] + '0');
+	}
+	_putchar('\n');
+	free(mul);
+}
+
+/**
+ * main - check the code.
+ * @argc : variable
+ * @argv : variable
+ * Return: 0 1
+ */
+
+int main(int argc, char **argv)
+{
+	int x, i, j, nl1, nl2, l, N1, N2, MLT, C;
+	int *mul;
+	char *n1 = *(argv + argc - 1);
+	char *n2 = *(argv + argc - 2);
+
+	if (argc != 3 || !_isdigit(n1) || !_isdigit(n2))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	nl1 = _strlen_recursion(n1);
+	nl2 = _strlen_recursion(n2);
+
+	l = nl1 + nl2;
+	mul = malloc(sizeof(int) * l);
+	if (mul == NULL)
+		return (1);
+
+	while (x <= l)
+	for (x = 0; x <= l; x++)
+		*(mul + x) = 0;
+
+	for (i = nl1 - 1; i >= 0; i--)
+	{
+		N1 = n1[i] - '0';
+		C = 0;
+		for (j = nl2 - 1; j >= 0; j--)
+		{
+			N2 = n2[j] - '0';
+			MLT = N1 * N2;
+			C = C + mul[i + j + 1] + MLT;
+			mul[i + j + 1] = C % 10; 
+			C = (C - (C % 10)) / 10;
+		}
+		mul[i + j + 1] += C;
+	}
+	_print(mul, l);
+	return (0);
+}
