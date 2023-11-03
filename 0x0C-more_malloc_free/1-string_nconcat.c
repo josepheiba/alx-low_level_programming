7f277b1ce827efeeab277bdabe390fc2579ff99a
Youssef Ibba <josepheiba@icloud.com>
2023-11-02 18:15:24 -0700

    oi

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > l2)
 		n = l2;
 
-	s = malloc(sizeof(char) * (l1 + n));
+	s = malloc(sizeof(char) * (l1 + n + 1));
 	if (s == NULL)
 		return (NULL);
 	x = 0;
