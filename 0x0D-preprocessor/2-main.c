9a0173e439187b8be92fb5a9235e6f651fb0959d
Youssef Ibba <josepheiba@icloud.com>
2023-11-04 07:16:57 -0700

    ok

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,36 +1,4 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _print - check code
- * @s: input
- * Return: check function declaration
- */
-
-void _print(char *s)
-{
-	int i;
-
-	i = 0;
-	while(s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar('\n');
-}
+#include <stdio.h>
 
 /**
  * main - check code
@@ -39,6 +7,6 @@ void _print(char *s)
 
 int main(void)
 {
-	_print(__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
