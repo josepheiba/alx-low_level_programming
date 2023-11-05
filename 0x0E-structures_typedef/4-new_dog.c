2f85b0aee1d65015d802f718f5428ce8e24c4e59
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 15:39:36 -0800

    ok

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * new_dog - check code
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: check function declaration
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t dog;
+	dog_t *dogP;
+	
+	dogP = &dog;
+	if (dogP != NULL)
+	{
+		dogP->name = name;
+		dogP->age = age;
+		dogP->owner = owner;
+		return (dogP);
+	}
+	else
+		return (NULL);
+}
