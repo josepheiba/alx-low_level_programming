e5b878c4042324614c39d77f0087ca385687f7e3
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 14:27:06 -0800

    ok

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,12 +18,7 @@ struct dog
 	char *owner;
 };
 
-struct dog_h
-{
-	char *name;
-	float age;
-	char *owner;
-};
+typedef struct dog dog_h;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
