7f602ac29b70ac6e302a37e0ef92a35758f9754b
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 15:48:44 -0800

    ok

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t dog;
 	dog_t *dogP;
-	
+
 	dogP = &dog;
-	if (dogP != NULL)
+	if (dogP->name == NULL || &(dogP->age) == NULL || dogP->owner == NULL)
+	{
+		return (NULL);
+	}
+	else
 	{
 		dogP->name = name;
 		dogP->age = age;
 		dogP->owner = owner;
 		return (dogP);
 	}
-	else
-		return (NULL);
+
 }
