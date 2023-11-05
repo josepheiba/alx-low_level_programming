fbac05124b6754ad0496fa6b09ee2f0777a17b57
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 07:28:54 -0800

    ok

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check the code
+ * @name: variable
+ * @age: variable
+ */
+
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
