e7e7307ede097064c70a82242aac436ee3f86bf6
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 07:47:59 -0800

    ok

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - check code
+ * @d: input
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Return: check function declaration
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
