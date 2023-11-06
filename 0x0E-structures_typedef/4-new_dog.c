f9cf831013c34476f20b6047cb5307ab8e173351
Youssef Ibba <josepheiba@icloud.com>
2023-11-05 16:44:25 -0800

    ok

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,40 @@
 #include <stdlib.h>
 
 /**
+ * _strlen_recursion - check code
+ * @s: input
+ * Return: check function declaration
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * *_strcpy - check code
+ * @dest: input
+ * @src: input
+ * Return: check function declaration
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		*(dest + i) = *(src + i);
+	i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - check code
  * @name: input
  * @age: input
@@ -12,19 +46,29 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dogP;
+	dog_t *dog;
 
-	dogP = malloc(sizeof(dog_t));
-	if (name == NULL || age < 0 || owner == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * _strlen_recursion(name) + 1);
+	if (dog->name == NULL)
 	{
+		free(dog);
 		return (NULL);
 	}
-	else
+	
+	dog->owner = malloc(sizeof(char) * _strlen_recursion(owner) + 1);
+	if (dog->name == NULL)
 	{
-		dogP->name = name;
-		dogP->age = age;
-		dogP->owner = owner;
-		return (dogP);
+		free(dog);
+		return (NULL);
 	}
-
+	dog->name = _strcpy(dog->name, name);
+	dog->owner = _strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
 }
