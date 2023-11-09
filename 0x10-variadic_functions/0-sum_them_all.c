0dc0b8309c1d8e6fd0f03995281428fac3a26a65
Youssef Ibba <josepheiba@icloud.com>
2023-11-09 07:10:50 -0800

    ok

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - check code.
+ * @n : variable
+ * Return: check declaration
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int number, sum, i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ptr, n);
+
+	number = n;
+	sum = 0;
+	i = 0;
+	while (i < n)
+	{
+		number = va_arg(ptr, unsigned int);
+		sum = sum + number;
+		i++;
+	}
+	return (sum);
+}
