243a17e139a65d781549cb63a8357cd13e99ed55
Youssef Ibba <josepheiba@icloud.com>
2023-11-09 08:27:46 -0800

    ok

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - check code.
+ * @separator : variable
+ * @n : variable
+ * Return: check declaration
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	char *string;
+
+	va_start(ptr, n);
+
+	i = 0;
+	while (i < n)
+	{
+		string = va_arg(ptr, char *);
+		printf("%s", string);
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
