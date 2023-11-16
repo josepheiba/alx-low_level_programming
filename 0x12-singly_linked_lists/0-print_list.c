faf3dc578d5e46c358a0d7bd4c36533736beda88
Youssef Ibba <josepheiba@icloud.com>
2023-11-16 07:59:36 -0800

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	unsigned long sz;
+
+	sz = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		sz++;
+	}
+	return (sz);
+}
