6db4ed540b798b69f82591bb7c65e69aec8c0871
Youssef Ibba <josepheiba@icloud.com>
2023-11-16 12:06:49 -0800

    ok

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * fun - check code.
+ * Return: check declaration
+ */
+
 void fun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
