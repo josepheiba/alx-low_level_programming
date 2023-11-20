d038c2c907464a626438ba817449f19f992f97e0
Youssef Ibba <josepheiba@icloud.com>
2023-11-20 14:21:17 -0800

    ok

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - check code.
+ * @head : variable
+ * @idx : variable
+ * @n : variable
+ * Return: check declaration
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *ptr;
+	listint_t *node;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	i = 0;
+	ptr = *head;
+	while (i < idx - 1)
+	{
+		ptr = ptr->next;
+		i++;
+		if (ptr == NULL && i != idx - 2)
+			return (NULL);
+	}
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->next = ptr->next;
+	ptr->next = node;
+
+	return (ptr);
+}
