5b2d1b74d0cdc9a1dca13b2dedecb53bbb1e0f95
Youssef Ibba <josepheiba@icloud.com>
2023-11-20 13:24:13 -0800

    ok

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,10 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr;
-
-	if ((*head) == NULL)
+	if (head == NULL || *head == NULL)
 		return;
-
-	while (*head != NULL)
-	{
-		ptr = (*head)->next;
-		free(*head);
-		*head = ptr;
-	}
+	if ((*head)->next != NULL)
+		free_listint2(&((*head)->next));
+	free(*head);
 	*head = NULL;
 }
