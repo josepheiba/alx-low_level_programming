725aba17646efc4fe24bd60088c12d7adf1f85ac
Youssef Ibba <josepheiba@icloud.com>
2023-11-20 12:20:22 -0800

    ok

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t  sz = 0;
+	size_t fuck;
 
-	while (h)
+	fuck = 0;
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		sz++;
+		fuck++;
 		h = h->next;
 	}
 
-	return (sz);
+	return (fuck);
 }
