9e59ee9515d0012a35554d20e377d891772c6a1c
Youssef Ibba <josepheiba@icloud.com>
2023-11-21 08:16:52 -0800

    ok

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - check code.
+ * @head : variable
+ * Return: check declaration
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned long sz, ok, i;
+	const listint_t *h;
+	const listint_t *ptr;
+	const listint_t *nully = NULL;
+
+	if (head == NULL)
+		exit(98);
+
+	h = head;
+	sz = ok = 0;
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		sz++;
+		if (h == nully)
+			break;
+		h = h->next;
+		ptr = h;
+		i = 0;
+		while (i < 100 && ok == 0 && ptr != NULL)
+		{
+			nully = ptr;
+			ptr = ptr->next;
+			if (ptr == h)
+				ok = 1;
+			i++;
+		}
+	}
+	if (ok == 1)
+	{
+		printf("-> %i\n", (nully->next)->n);
+	}
+	return (sz);
+}
