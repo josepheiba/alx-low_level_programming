98c81e292ec4215a0a71324af2f8e3272cd348a3
Youssef Ibba <josepheiba@icloud.com>
2023-11-21 10:43:15 -0800

    ok

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - check code.
+ * @head : variable
+ * Return: check declaration
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			return (head);
+		}
+	}
+	return (NULL);
+}
