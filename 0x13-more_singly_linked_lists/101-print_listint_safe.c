d7b397767708c8117037341c559db99bd3918f71
Youssef Ibba <josepheiba@icloud.com>
2023-11-21 09:27:49 -0800

    ok

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,28 +5,24 @@
  * @head : variable
  * Return: check declaration
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	unsigned int sz;
-	long int df;
+    size_t num = 0;
+    long int diff;
 
-	if (head == NULL)
-		exit(98);
+    while (head)
+    {
+        diff = head - head->next;
+        num++;
+        printf("[%p] %d\n", (void *)head, head->n);
+        if (diff > 0)
+            head = head->next;
+        else
+        {
+            printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+            break;
+        }
+    }
 
-	sz = 0;
-	while (head != NULL)
-	{
-		df = head - head->next;
-		printf("[%p] %d\n", (void *)head, head->n);
-		sz++;
-		if (df > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
-			break;
-		}
-	}
-	return (sz);
+    return (num);
 }
