4187e6a19d78f8c0afdf5d1d32cdba50fea76dd1
Youssef Ibba <josepheiba@icloud.com>
2023-11-20 17:58:27 -0800

    ok

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	i = 0;
 	ptr = *head;
-	while (i < index && ptr != NULL)
+	while (i < index && ptr != NULL && ptr->next != NULL)
 	{
 		if (i != index - 1)
 			ptr = ptr->next;
