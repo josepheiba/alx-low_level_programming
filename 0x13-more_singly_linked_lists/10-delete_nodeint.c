012b9f45bbc7906b125cce235c1aa81dc7e0140c
Youssef Ibba <josepheiba@icloud.com>
2023-11-20 16:11:36 -0800

    ok

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - check code.
+ * @head : variable
+ * @index : variable
+ * Return: check declaration
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *ptr;
+	listint_t *ptr2;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+		return (1);
+	}
+
+	i = 0;
+	ptr = *head;
+	while (i < index && ptr != NULL)
+	{
+		if (i != index - 1)
+			ptr = ptr->next;
+		else
+		{
+			ptr2 = ptr->next;
+			ptr->next = (ptr->next)->next;
+			free(ptr2);
+			return (1);
+
+		}
+		i++;
+	}
+	return (-1);
+}
