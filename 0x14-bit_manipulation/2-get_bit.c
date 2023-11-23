11e2ff6b714a9f97abace448db00d97fede93b51
Youssef Ibba <josepheiba@icloud.com>
2023-11-23 11:44:52 -0800

    ok

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - check code.
+ * @n : variable
+ * @index : variable
+ * Return: check declaration
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int one, num;
+
+	one = 1;
+	n = n >> index;
+	num = n & one;
+	return (num);
+}
