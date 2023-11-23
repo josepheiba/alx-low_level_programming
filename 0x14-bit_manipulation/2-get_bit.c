7f8df276384dc8fd38860da25fb4bad5d6ba8d55
Youssef Ibba <josepheiba@icloud.com>
2023-11-23 11:51:59 -0800

    ok

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,11 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int one, num;
-	int bruh;
 
+	if (index > sizeof(n) * 8)
+		return (-1);
 	one = 1;
 	n = n >> index;
 	num = n & one;
-	bruh = num;
-	return (bruh);
+	return (num);
 }
