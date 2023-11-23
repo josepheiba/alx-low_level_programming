fa32ac9bc1dca4ceeb0baecb538cf15133f4a39c
Youssef Ibba <josepheiba@icloud.com>
2023-11-23 10:24:47 -0800

    ok

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - check code.
+ * @b : variable
+ * Return: check declaration
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned long num;
+	int i, n;
+	unsigned int TwoPwX;
+
+	i = 0;
+	while (*(b + i) != '\0')
+		i++;
+	i--;
+
+	TwoPwX = 1;
+	num = 0;
+	while (i >= 0)
+	{
+		n = *(b + i) - '0';
+		if (n != 0 && n != 1)
+			return (0);
+		num = num + n * TwoPwX;
+		i--;
+		TwoPwX = TwoPwX << 1;
+	}
+	return (num);
+}
