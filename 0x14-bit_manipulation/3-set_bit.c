eef9fec67dfe50c94fc9799fba6b4323f4c78fcd
Youssef Ibba <josepheiba@icloud.com>
2023-11-23 11:59:51 -0800

    ok

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int one;
 
-	if (index > sizeof(*n) * 8)
+	if (index > sizeof(*n) * 8 || n == NULL)
 		return (-1);
 	one = 1;
 	one = one << index;
