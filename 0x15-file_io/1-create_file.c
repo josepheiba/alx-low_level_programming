5669bde53542e4859aa0ae6b0b62e7cd9edf5efb
Youssef Ibba <josepheiba@icloud.com>
2023-11-27 13:21:04 -0800

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,23 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, pt, i;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	i = 0;
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content != NULL)
+	{
+		i = 0;
+		while (text_content[i] != '\0')
+			i++;
 
-	pt = write(fd, text_content, i);
-	if (pt < 0)
-		return (-1);
+		pt = write(fd, text_content, i);
+		if (pt == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
