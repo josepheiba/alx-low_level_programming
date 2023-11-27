166eaf0b819a3375702a0c2236880620c3318559
Youssef Ibba <josepheiba@icloud.com>
2023-11-27 15:48:21 -0800

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,21 +26,21 @@ int main(int ac, char **av)
 	if (fo == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	while ((fr = read(fi, bf, 1024)) != 0)
+	while ((fr = read(fi, bf, 1024)) > 0)
 	{
-		if (fr == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
 		fw = write(fo, bf, fr);
 		if (fw == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
+	if (fr == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	fci = close(fi);
 	if (fci == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fi), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fi), exit(100);
 	fco = close(fo);
 	if (fco == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fo), exit(100);
-	return (1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fo), exit(100);
+	return (0);
 }
