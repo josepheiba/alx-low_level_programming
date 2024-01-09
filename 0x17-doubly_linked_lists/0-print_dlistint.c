f0cdc7808f0e7e34f6b1967ce877654a9efcc121
Joseph Eiba <josepheiba@icloud.com>
2024-01-09 17:10:06 +0100

    ok

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - check the code
+ * @h - input
+ * Return: check code
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int n;
+	dlistint_t *head;
+
+	n = 0;
+	head = (dlistint_t *)h;
+
+	while (head != NULL) {
+		printf("%d\n", head->n);
+		head = head->next;
+		n++;
+	}
+	return (n);
+}
