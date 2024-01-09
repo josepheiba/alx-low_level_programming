861c2855becb42967aeea949512d2bfa7edb87a4
Joseph Eiba <josepheiba@icloud.com>
2024-01-09 19:09:36 +0100

    ok

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,10 +19,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (dptr != NULL)
 	{
+		if (i == index)
+			return (dptr);
 		dptr = dptr->next;
 		i++;
-		if (i == index)
-			break;
 	}
-	return (dptr);
+	return (NULL);
 }
