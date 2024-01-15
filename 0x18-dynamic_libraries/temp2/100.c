7c78e58c4f38abc9e28f9f17ac23758fef1a9a28
Youssef Ibba <josepheiba@icloud.com>
2024-01-15 12:27:51 -0800

    ok

---
+++ 0x18-dynamic_libraries/temp2/100.c
@@ -0,0 +1,26 @@
+#include "main2.h"
+
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+int div(int a, int b)
+{
+	return(a / b);
+}
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
