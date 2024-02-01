f19169cdc8bb29f72cae55f92738f181f9bf8276
Joseph Eiba <josepheiba@icloud.com>
2024-02-01 23:25:53 +0100

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,17 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	hash_node_t **array;
+	unsigned int i;
 
 	if (size <= 0)
-	{
 		return (0);
-	}
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == 0)
-	{
 		return (0);
-	}
 
 	array = malloc(sizeof(hash_node_t *) * size);
 	if (array == 0)
@@ -28,6 +25,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_table);
 		return (0);
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
 	new_table->size = size;
 	new_table->array = array;
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+# include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_set - fun
+ *
+ * @ht: in
+ * @key: in
+ * @value: in
+ * Return: out
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (key == 0)
+		return (0);
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == 0)
+		return (0);
+
+	node->key = (char *)key;
+	strcpy(node->value, value);
+
+	if (ht->array[index] == 0)
+	{
+		node->next = 0;
+		ht->array[index] = node;
+		return (1);
+	}
+
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
+}
