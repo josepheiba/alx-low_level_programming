7b57eb871fddc3bdab70fb2e6f4bff4326ff72cb
Youssef Ibba <josepheiba@icloud.com>
2024-02-01 11:20:31 -0800

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table;
-    hash_node_t **array;
+	hash_table_t *new_table;
+	hash_node_t **array;
 
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == 0)
-    {
-        return (0);
-    }
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == 0)
+	{
+		return (0);
+	}
 
-    array = malloc(sizeof(hash_node_t) * size);
-    if (array == 0)
-    {
-        return (0);
-    }
+	array = malloc(sizeof(hash_node_t) * size);
+	if (array == 0)
+	{
+		return (0);
+	}
 
-    new_table->size = size;
-    new_table->array = array;
+	new_table->size = size;
+	new_table->array = array;
 
-    return (new_table);
+	return (new_table);
 }
