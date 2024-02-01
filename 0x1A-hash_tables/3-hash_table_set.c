27d614f4525882def8e7f44b82b928fe29c2ef6f
Joseph Eiba <josepheiba@icloud.com>
2024-02-02 00:07:28 +0100

    ok

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,13 +13,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (key == 0 || ht == 0 || ht->size == 0)
+	if (key == 0 || ht == 0 || ht->size == 0 || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (strlen(key) == 0)
-		return (0);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == 0)
