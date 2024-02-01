15e3505aaa94206d64f356800d662358d13bda01
Joseph Eiba <josepheiba@icloud.com>
2024-02-01 20:08:42 +0100

    ok

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,7 +5,7 @@
  *
  * @size: in
  * Return: out
- */ 
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
   hash_table_t *new_table;
@@ -14,17 +14,17 @@ hash_table_t *hash_table_create(unsigned long int size)
   new_table = malloc(sizeof(hash_table_t));
   if (new_table == 0)
   {
-    return 0;
+    return (0);
   }
 
   array = malloc(sizeof(hash_node_t) * size);
   if (array == 0)
   {
-    return 0;
+    return (0);
   }
 
   new_table->size = size;
   new_table->array = array;
 
-  return new_table;
+  return (new_table);
 }
