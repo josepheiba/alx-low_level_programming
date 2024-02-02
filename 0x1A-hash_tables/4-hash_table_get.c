ff89e9171b6351f53501a2c23d06f072b9af7ea1
Joseph Eiba <josepheiba@icloud.com>
2024-02-02 03:07:18 +0100

    ok

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+# include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - fun
+ *
+ * @ht: in
+ * @key: in
+ * Return: out
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *pointer;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	pointer = ht->array[index];
+	while (pointer != 0)
+	{
+		if (strcmp(key, pointer->key) == 0)
+		{
+			return (pointer->value);
+		}
+	}
+	return (0);
+}
