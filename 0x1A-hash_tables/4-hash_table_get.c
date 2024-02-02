f70104bb918db9d1ae2eed48289ff4c4d3cfb3b2
Joseph Eiba <josepheiba@icloud.com>
2024-02-02 03:08:59 +0100

    ok

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *pointer;
 
+	if (key == 0 || ht == 0 || ht->size == 0 || strlen(key) == 0)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	pointer = ht->array[index];
 	while (pointer != 0)
